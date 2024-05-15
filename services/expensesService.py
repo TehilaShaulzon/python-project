5eb11ef05be7bbb4b93aa624a43102352a5d5fa2
TehilaShaulzon <t8472959@gmail.com>
2024-05-16 00:05:17 +0300

    fix automatic id in expenses controller

--- services/expensesService.py
+++ services/expensesService.py
@@ -5,6 +5,7 @@ from models.expenses import Expenses
 from models.users import User
 
 collection = db['expenses']
+expenses_id = (collection.find({}, { 'id': 1}).sort('id', -1)[0])["id"]
 
 
 async def getExpenses(user_id):
@@ -17,6 +18,9 @@ async def getExpenses(user_id):
     return expenses
 async def add_new_expenses(new_expenses:Expenses):
     try:
+        global expenses_id
+        expenses_id += 1
+        new_expenses.id = expenses_id
         collection.insert_one(new_expenses.dict())
     except Exception as e:
         raise e
